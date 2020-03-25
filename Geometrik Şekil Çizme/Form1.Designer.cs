1d916047f544ef322bb07adf0650c18ad9180949
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:05:46 +0300

    Initial commit

---
+++ Geometrik Şekil Çizme/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Geometrik_Şekil_Çizme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Çember",
+            "Elips",
+            "Kare",
+            "Dikdörtgen",
+            "Yazı yaz",
+            "Yay",
+            "Serbest Yay",
+            "Serbest Çizim",
+            "Çizgi",
+            "Çeyrek Çember",
+            "Çokgen"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(604, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Çizilmesini İstediğiniz Şekli Seçiniz";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(423, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(193, 268);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 332);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Geometrik Şekil Çizme/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geometrik_Şekil_Çizme
+{
+    public partial class Form1 : Form
+    {
+        #region Definitions
+
+        Graphics G;
+        SolidBrush Fırça = new SolidBrush(Color.Black);
+        Pen Kalem = new Pen(Color.Black, 5);
+        Font Fontum = new Font("Georgia", 20);
+
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Çember":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawEllipse(Kalem,200,100,80,80);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Elips":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawEllipse(Kalem, 200, 100, 80, 60);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Kare":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawRectangle(Kalem, 200, 100, 100, 100);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Dikdörtgen":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawRectangle(Kalem, 200, 100, 100, 80);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Yazı yaz":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawString("Industryolog Akademi", Fontum, Fırça, 50, 50);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Yay":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawBezier(Kalem,100,100,130,150,80,90,250,95);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Serbest Yay":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawBezier(Kalem, 100, 75, 135, 100, 170, 100, 200, 75);
+                        G.Dispose();
+                        break;
+                    }
+
+                case "Serbest Çizim":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        Point[] p1 = { new Point(100, 70), new Point(125, 100), new Point(150, 150), new Point(200, 60) };
+                        G.DrawCurve(Kalem, p1, 2);
+                        G.Dispose();
+                        break;
+                    }
+                case "Çizgi":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawLine(Kalem, 100, 100, 300, 100);
+                        G.Dispose();
+                        break;
+                    }
+                case "Çeyrek Çember":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        G.DrawPie(Kalem, 100, 100, 150, 100, 200, 100);
+                        G.Dispose();
+                        break;
+                    }
+                case "Çokgen":
+                    {
+                        G = this.CreateGraphics();
+                        G.Clear(this.BackColor);
+                        Point[] p2 = { new Point(150, 100), new Point(200, 150), new Point(200, 200), new Point(150, 250), new Point(100, 150), new Point(100, 110), new Point(300,250) };
+                        G.DrawPolygon(Kalem, p2);
+                        G.Dispose();
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+}
